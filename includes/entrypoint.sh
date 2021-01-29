12803b43f644c5d6718bccb640d6180f1728e746
Simon Sharman <simon.sharman@telxl.com>
2021-01-29 11:44:32 +0000

    changes

--- includes/entrypoint.sh
+++ includes/entrypoint.sh
@@ -6,5 +6,8 @@ IP=`hostname -i`
 #Set Metida and Signal Address for PJSIP
 sed -i "s/CHANGEMEIP/$IP/g" /etc/asterisk/pjsip.conf
 
+#This is a test
+echo "WASSSSSSUP!!!!!!"
+
 #Start asterisk
 asterisk -vvvvvvfp
